e2e34cc0c0b28f5e12c57c7fb1fd78d65a7a32cd
imashwani <ashwani.p30@gmail.com>
2019-04-03 22:17:55 +0530

    worked on bookmark Controller

---
+++ app/controllers/bookmarkController.js
@@ -0,0 +1,49 @@
+const Bookmark = require('../models/Bookmark');
+
+exports.me = async ( req, res ) => {
+    return res.send({ ...req.user[0].toJSON() });
+};
+//delete, add, getMyBookList, is bookmarked(find)
+
+exports.getMyBookList=async (req,res)=>{
+    try {
+        // const data = await Bookmark.query().where('user_id',req.params.userid);
+        const userObj={ ...req.user[0].toJSON() };
+        const data = await Bookmark.query().where('user_id',userObj.id);
+
+        return res.send({
+            data,
+            message: "Data sent successfully"
+        })
+    } catch(err) {
+        res.status(500).send({ message: err.message });
+    }
+};
+
+exports.find=async (req,res)=>{
+    try {
+        const userObj={ ...req.user[0].toJSON() };
+        const data = await Bookmark.query().where('article_id',req.params.articleid).andWhere({user_id:userObj.id});
+
+        return res.send({
+            data,
+            total:data.length,
+            message: "Data sent successfully"
+        })
+    } catch(err) {
+        res.status(500).send({ message: err.message });
+    }
+};
+
+exports.delete = async ( req, res ) => {
+    try {
+        const data = await Bookmark.query().deleteById( req.params.id );
+
+        return res.send({
+            data,
+            message: "Bookmark updated successfully"
+        })
+    } catch(err) {
+        res.status(500).send({ message: err.message });
+    }
+};
