560bcee3cb0c5d37d526f43564e398e08c759007
dsc712 <devyendushekhar712@gmail.com>
2019-04-03 22:40:27 +0530

    push show.js file

--- pages/show.js
+++ pages/show.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import axios from "axios";
 import { withRouter } from "next/router";
 import App from "../components/layouts/App";
-import {Card, Icon } from "antd";
+import {Card, Icon, Affix, Row, Col } from "antd";
 import moment from "moment";
 import ReactHTMLParser from "react-html-parser";
 
@@ -26,30 +26,50 @@ class Show extends Component {
             <App style={{display: "flex", flexDirection: "row", backgroundColor: "#fff"}}>
                 <img style={{"width": "100vw", "marginTop": "-40px", overflowX: "hidden"}} alt="example"
                      src={featured_image}/>
-                <Card style={{}}>
-                    <Card style={{"width": "85vw", "margin": "0 auto"}}>
-                        <h1 style={{"fontSize": "3em"}}>{title}</h1>
-                        <h4 style={{"color": "#333333", "textAlign": "justify"}}>
-                            <span style={{"marginRight": "20px"}}><Icon type="clock-circle"
-                                                                        style={{"marginRight": "3px"}}/>{reading_time}</span>
-                            <span><Icon type="calendar"
-                                        style={{"marginRight": "3px"}}/>{moment(created_at).fromNow()}</span>
-                        </h4>
-                    </Card>
-                    <div
-                        style={{
-                            textAlign: "justify",
-                            margin: "auto",
-                            padding: "100px",
-                            fontSize: "1.5em"
-                        }}
-                    >
-                        {ReactHTMLParser(body)}
-                    </div>
+                <Card>
+                    <Row>
+                        <Col span={1}>
+                            <Affix offsetTop={200}>
+                                <div style={{ height: "30vh", fontSize: "24px", display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
+                                    <Icon size="large" type="share-alt" />
+                                    <Icon size="large" type="facebook" />
+                                    <Icon size="large" type="twitter" />
+                                </div>
+                            </Affix>
+                        </Col>
+
+                        <Col span={23}>
+                            <Card style={{"width": "85vw", "margin": "0 auto"}}>
+                                <h1 style={{"fontSize": "3em"}}>{title}</h1>
+                                <h4 style={{"color": "#333333", "textAlign": "justify"}}>
+                                <span style={{"marginRight": "20px"}}>
+                                    <Icon type="clock-circle" style={{"marginRight": "3px"}}/>{reading_time}
+                                </span>
+                                        <span>
+                                    <Icon type="calendar" style={{"marginRight": "3px"}}/>{moment(created_at).fromNow()}
+                                </span>
+                                </h4>
+                            </Card>
+
+                            <div
+                                style={{
+                                    textAlign: "justify",
+                                    margin: "auto",
+                                    padding: "100px",
+                                    fontSize: "1.5em"
+                                }}
+                            >
+                                {ReactHTMLParser(body)}
+                            </div>
+                            </Col>
+                    </Row>
                 </Card>
+
                 <Card>
                     <Icon type="edit"/>
                 </Card>
+
+
             </App>
         );
     }
